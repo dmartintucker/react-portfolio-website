eaf446cac0f5576037d56f030bef73a67e92332a
tuckerdm <dmartintucker@gmail.com>
2022-04-06 23:58:22 -0500

    started building homepage

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Logo</h2>
+      <div className="navbar-links">
+        <p>My Why</p>
+        <p>Case Studies</p>
+        <p>My Projects</p>
+        <button>Get In Touch</button>
+      </div>
+    </nav>
+  );
+}
---
+++ src/pages/project.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <div>
+      <h3>Project Route</h3>
+    </div>
+  );
+}
